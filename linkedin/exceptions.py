843b23b5cdca132b518d9ade3bb03ed99ef48f30
Samuel Marks <samuelmarks@gmail.com>
2014-12-12 21:48:18 +1100

    PEP8

--- linkedin/exceptions.py
+++ linkedin/exceptions.py
@@ -3,35 +3,44 @@
 class LinkedInError(Exception):
     pass
 
+
 class LinkedInBadRequestError(LinkedInError):
     pass
+
 
 class LinkedInUnauthorizedError(LinkedInError):
     pass
 
+
 class LinkedInPaymentRequiredError(LinkedInError):
     pass
+
 
 class LinkedInNotFoundError(LinkedInError):
     pass
 
+
 class LinkedInConflictError(LinkedInError):
     pass
+
 
 class LinkedInForbiddenError(LinkedInError):
     pass
 
+
 class LinkedInInternalServiceError(LinkedInError):
     pass
 
+
 ERROR_CODE_EXCEPTION_MAPPING = {
-            400: LinkedInBadRequestError,
-            401: LinkedInUnauthorizedError,
-            402: LinkedInPaymentRequiredError,
-            403: LinkedInForbiddenError,
-            404: LinkedInNotFoundError,
-            409: LinkedInForbiddenError,
-            500: LinkedInInternalServiceError}
+    400: LinkedInBadRequestError,
+    401: LinkedInUnauthorizedError,
+    402: LinkedInPaymentRequiredError,
+    403: LinkedInForbiddenError,
+    404: LinkedInNotFoundError,
+    409: LinkedInForbiddenError,
+    500: LinkedInInternalServiceError}
+
 
 def get_exception_for_error_code(error_code):
     return ERROR_CODE_EXCEPTION_MAPPING.get(error_code, LinkedInError)
